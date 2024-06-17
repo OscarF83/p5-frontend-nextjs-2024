132212a8dfccf6e70ffdc8d8fc925a44cfea744d
Oscar <o_fonte@hotmail.com>
2024-06-17 17:11:52 +0200

    MessageForm reading values with addMessage

--- my-first-nextjs-app/components/MessageForm.tsx
+++ my-first-nextjs-app/components/MessageForm.tsx
@@ -1,19 +1,26 @@
 "use client";
-
+import { readJson } from "@/scripts/readJson";
 import React, { FormEventHandler, useRef } from "react";
 
 export default function MessageForm() {
-  const formRef = useRef<HTMLFormElement | null>(null);
+  const nickRef = useRef<HTMLInputElement | null>(null);
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const messageRef = useRef<HTMLInputElement | null>(null);
 
   const addMessage: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    console.log("Añadiendo mensaje");
+    console.log(nickRef.current!.value);
+    console.log(nameRef.current!.value);
+    console.log(messageRef.current!.value);
+    nickRef.current!.value = "";
+    nameRef.current!.value = "";
+    messageRef.current!.value = "";
+      
   };
 
   return (
     <form
       className="flex flex-col"
-      ref={formRef}
       onSubmit={addMessage}
     >
       <div className="flex flex-row pb-3">
@@ -21,11 +28,13 @@ export default function MessageForm() {
         type="text"
         name="nick"
         className="border shadow px-2 mr-2 rounded-lg"
+        ref={nickRef}
       />
       <input
         type="text"
         name="name"
         className="border shadow px-2 mr-2 rounded-lg"
+        ref={nameRef}
       />
       </div>
       <div className="flex flex-row">
@@ -33,6 +42,7 @@ export default function MessageForm() {
         type="text"
         name="message"
         className="border shadow px-2 mr-2 rounded-lg size-full h-10 self-center"
+        ref={messageRef}
       />
       <button className="border shadow font-bold py-1 px-2 my-2 rounded-lg bg-stone-200 hover:bg-stone-400">
         Send
