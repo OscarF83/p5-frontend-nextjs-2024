025edc7a7b275ab1bda434acf85b468c232fe74a
Oscar <o_fonte@hotmail.com>
2024-06-19 21:46:10 +0200

    useRouter Forum + shadcn button link

--- my-first-nextjs-app/app/forum/page.tsx
+++ my-first-nextjs-app/app/forum/page.tsx
@@ -5,13 +5,13 @@ import {readJson} from "@/lib/readJson";
 export default async function Home() {
   const messagesList = await readJson();
   return (
-    <main className="snap-y py-20">
-      <div className="snap-end px-40 py-3 flex flex-col gap-2">
+    <main className="py-20">
+      <div className="px-40 py-3 flex flex-col gap-2">
       {messagesList.map((a) => (
           <Message key={a.id} message={a} />
       ))}
     </div>
-    <div className="snap-end px-20 py-6">
+    <div className="px-20 py-6">
         <MessageForm/>
       </div>
     </main>
--- my-first-nextjs-app/components/Header.tsx
+++ my-first-nextjs-app/components/Header.tsx
@@ -2,9 +2,10 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default function Header() {
-  
+    const router = useRouter();
   return (
     <header className="fixed top-0 left-0 right-0 h-20 bg-stone-700 text-white flex flex-row items-center px-4">
       <Link href="/">
@@ -12,6 +13,13 @@ export default function Header() {
           Multi-thematic forum
         </div>
       </Link>
+      <Button
+        variant="link"
+        className="text-white"
+        onClick={() => router.push("/forum")}
+      >
+        Forum
+      </Button>
     </header>
   );
 }
